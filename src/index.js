3f452b9762d1665ee75eba8dd15ca8ea6f1b65b8
IcyRossaMac <IcyRossa@gmail.com>
2019-07-22 21:03:07 +0100

    ReactProvider and Store added to Index

--- src/index.js
+++ src/index.js
@@ -4,10 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
 import "./index.css";
+import configureStore from "./redux/configureStore";
+import { Provider as ReactProvider } from "react-redux";
+
+const store = configureStore;
 
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReactProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReactProvider>,
   document.getElementById("app")
 );
